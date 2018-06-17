0a2ba3ce1169aeaba845238f22f03e1884e2f376
Anthea Harvey-Brown <tharveybrown@gmail.com>
2018-06-17 15:21:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function shout(string) {
   return string.toUpperCase()
 }
 
-<!-- describe('whisper(string)', function() {
+/* describe('whisper(string)', function() {
   it('receives one argument and returns it in all lowercase', function() {
     expect(whisper('HELLO')).toEqual('hello')
   })
