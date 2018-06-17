b3db6b42dfec2d2697ab743ea6b6478eccd7f824
Anthea Harvey-Brown <tharveybrown@gmail.com>
2018-06-17 15:31:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if {string.toLowerCase() === string)
+  if (string.toLowerCase() === string) {
+    return("I can't hear you!")
+  }
+  
+  if (string.toUpperCase() === string) {
+    return
+  }
 }
 
 describe('sayHiToGrandma(string)', function() {
